9530f89bed03a840ba55d30640452ec5bfb95479
nikita <nikitaagarwal8755@gmail.com>
2023-05-29 23:11:27 +0530

    Add

---
+++ MCQ/objHard.js
@@ -0,0 +1,12 @@
+let temp = {
+    a:10,
+    b:20,
+    sum(){
+        return this.a + this.b;
+    },
+    multi : ()=>{
+        return this.a * this.b;
+    }
+}
+console.log(temp.sum())
+console.log(temp.multi())
