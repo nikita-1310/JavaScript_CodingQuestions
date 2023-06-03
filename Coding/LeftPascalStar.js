20661a8c2d14b576ca514f56c50b29294e0c859d
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-06-03 23:25:00 +0530

    Update LeftPascalStar.js

--- Coding/LeftPascalStar.js
+++ Coding/LeftPascalStar.js
@@ -1,1 +1,21 @@
 
+var n = 5;
+var str = "";
+for(let i=1; i<=n; i++){
+  for(let j=0; j<n-i; j++){
+    str += " ";
+  }
+   for(let k=0; k<i; k++){
+    str+= "*";
+  }
+  str +="\n"
+for(let i=1; i<=n-1; i++){
+  for(let j=0; j<i; j++){
+    str += " ";
+  }
+  for(let k=0; k<n-i; k++){
+  str += "*"
+}
+  str +="\n";
+}
+console.log(str);
